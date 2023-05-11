f675ec30a192937f051e9f47bf78153b49fd58f7
CLENALIO <caroline.le.nalio@gmail.com>
2023-05-11 22:53:58 +0200

    style a finaliser

--- src/components/Maths/JeuMaths.jsx
+++ src/components/Maths/JeuMaths.jsx
@@ -3,16 +3,24 @@ import { useState } from "react";
 
 const JeuMaths = ({setCount}) => {
 
+    const [classeMath, setClasseMath] = useState("")
+    const [titleMath, setTitleMath] = useState("Quel est le résultat de ce calcul?")
+
     const [value1, setvalue1] = useState(Math.floor(Math.random()* (90 - 1)) + 1);
     const [value2, setvalue2] = useState(Math.floor(Math.random()* (90 - 1)) + 1);
-    const [calcul, setCalcul] = useState("-")
+    const [calcul, setCalcul] = useState("-");
 
-    const handleClickMath = function (e) {
-        e.preventDefault()
+    const operators = ["-", "+", "*"];
+    const randomIndex = Math.floor(Math.random() * operators.length);
+    const randomOperator = operators[randomIndex];
+
+    const handleClickNext = () => {
         setCount (c => c + 1)
         setvalue1(Math.floor(Math.random()* (50 - 15)) + 15);
         setvalue2(Math.floor(Math.random()* (14 - 1)) + 1);
-        setCalcul(["-", "+", "*"].random());
+        setCalcul(randomOperator);
+        setClasseMath("");
+        setTitleMath("Quel est le résultat de ce calcul?");
     }
 
     const calculator = (a, b, c) => {
@@ -30,27 +38,33 @@ const JeuMaths = ({setCount}) => {
    
     const goodAnswer = calculator(value1, calcul, value2);
 
-    const [classeMath, setClasseMath] = useState("")
-
     const [answer, setAnswer] = useState ("");
+
     function handleChangeCalc(e) {
         setAnswer(e.target.value);
-        if (e.target.value == goodAnswer) {
-            setClasseMath("bonneReponse")
-        }
-        else {
-            setClasseMath("mauvaiseReponse")
-        }
       }
 
+    const handleClickOK = () => {
+      if (answer == goodAnswer) {
+        setClasseMath("bonneReponse");
+        setTitleMath("Bravo!");
+      }
+      else {
+        setClasseMath("mauvaiseReponse");
+        setTitleMath("Oups!");
+      }
+    }
 
     return (
         <div className="jeu-maths">
             <div className ="question">
-                <h3>Quel est le résultat de ce calcul?</h3>
+                <h3>{titleMath}</h3>
                 <p>{value1} {calcul} {value2} = </p>
                 <input className= {classeMath} type="text" value={answer} onChange={handleChangeCalc}/>
-                <button onClick={handleClickMath}>Suivant</button>
+                <div className= "buttons">
+                <button className = "boutonOKJeu" onClick={handleClickOK}>OK</button>
+                <button className = "boutonNextJeu" onClick={handleClickNext}>Suivant</button>
+                </div>
             </div>
         </div>
     );
