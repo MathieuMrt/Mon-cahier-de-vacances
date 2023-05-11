9e9da150f04b792edc0f4a4048cd6beea7bd21ea
chloeafp <chloeafp@gmail.com>
2023-05-11 17:46:09 +0200

    CV9 consigne end FR

--- src/components/Francais/Francais.jsx
+++ src/components/Francais/Francais.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Header from '../Header/Header.jsx';
 import Footer from '../Footer/Footer.jsx';
+import ConsigneFr from './ConsigneFr.jsx';
+import EndFr from './EndFr.jsx';
 
-const Francais = ({color}) => {
+const Francais = () => {
     return (
         <div className="francais">
             <Header color="francaisColor" subject="Français"/>
+            <ConsigneFr />
             <Footer color="francaisColor"/>
         </div>
     );
