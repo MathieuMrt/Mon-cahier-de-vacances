27bb192d846f495519df45d6f2ff25912dca309a
chloeafp <chloeafp@gmail.com>
2023-05-11 20:29:08 +0200

    function handleClick affichage blended mot

--- src/components/Francais/JeuFr.jsx
+++ src/components/Francais/JeuFr.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import coral from "../../assets/images/coral.png";
+import { useState } from "react";
 
-const mots = [
+const JeuFr = () => {
+  const mots = [
     "baleine",
     "parasol",
     "vacances",
@@ -12,33 +15,46 @@ const mots = [
     "valise",
     "paysage",
   ];
+  const [index, setIndex] = useState(0);
 
-const JeuFr = () => {
-    
-      const blendLetters = (mots) => {
-        var blendedMots = mots.map((mot) => {
-            const letter = mot.split('');
-    
-            for (let i = letter.length-1; i> 0 ; i--) {
-                var j = Math.floor(Math.random() * (i + 1));
-                var temp = letter[i];
-                letter[i] = letter[j];
-                letter[j] = temp;
-    
-            }
-        const blendedMot = letter.join('');
-    
-        return blendedMot;
-    
-        })
-        return blendedMots;
+  const handleClickJeuFr = () => {
+    console.log("lol");
+    setIndex((e) => (e + 1) % blendedArray.length);
+  };
+
+  console.log(mots[0]);
+
+  const blendLetters = (mots) => {
+    var blendedMots = mots.map((mot) => {
+      const letter = mot.split("");
+
+      for (let i = letter.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = letter[i];
+        letter[i] = letter[j];
+        letter[j] = temp;
       }
+      const blendedMot = letter.join("");
 
+      return blendedMot;
+    });
+    return blendedMots;
+  };
+  const blendedArray = blendLetters(mots);
+  console.log(blendedArray);
 
-  console.log(blendLetters(mots))
-
-
-  return  <div></div>;
+  return (
+    <div className="Consigne">
+      <div className="JeuFr">
+        <img src={coral} alt="coral" className="Consigne_img" />
+        <p className="JeuFr_mot">{blendedArray[index]}</p>
+        <input type="text" className="JeuFr_input"></input>
+      </div>
+      <button className="Francais_button" onClick={handleClickJeuFr}>
+        Suivant
+      </button>
+    </div>
+  );
 };
 
 export default JeuFr;
