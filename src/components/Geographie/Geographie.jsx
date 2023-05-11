fab66ecb42478a9b56e0386d713340a9c89ca2bf
chloeafp <chloeafp@gmail.com>
2023-05-11 16:04:55 +0200

    CV2 - integration Consigne

--- src/components/Geographie/Geographie.jsx
+++ src/components/Geographie/Geographie.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
+/* import React from 'react'; */
 import Header from '../Header/Header.jsx';
 import Footer from '../Footer/Footer.jsx';
 import Consigne from './Consigne.jsx';
-import ReponseCapitale from './ReponseCapitale.jsx';
+/* import ReponseCapitale from './ReponseCapitale.jsx';
 import ReponseDrapeau from './ReponseDrapeau.jsx';
-import End from './End.jsx';
+import End from './End.jsx'; */
 
 const Geographie = () => {
     return (
         <div className="geographie">
             <Header/>
+            <Consigne />
             <Footer/>
         </div>
     );
